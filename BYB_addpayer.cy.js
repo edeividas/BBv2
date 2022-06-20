aef469e6fc6e31e2b735e9f65a31df17c616d620
edeividas <103195877+edeividas@users.noreply.github.com>
2022-06-20 10:36:03 +0300

    naujasssss

--- BYB_addpayer.cy.js
+++ BYB_addpayer.cy.js
@@ -1,5 +1,6 @@
-describe('add player to BYB', () => {
-    it('adds player and statistic', () => {
-        cy.visit('https://widgets-bm.dev.digitalsportstech.com/betbuilder?sb=test&demo=test&user=deividas1')
+describe('BetSlip test bet', () => {
+    it('Open betslip,place 1 dollar bet and confirms order', () => {
+        cy.visit('https://widgets-bm.dev.digitalsportstech.com/betbuilder?sb=test&deividas=true&shwExp=true')
     })
+
 })
