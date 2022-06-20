450138157d48229be0b9f7a3059ef538b1c2dc21
edeividas <103195877+edeividas@users.noreply.github.com>
2022-06-20 10:03:00 +0300

    nuajaaaa

---
+++ BYB_addpayer.cy.js
@@ -0,0 +1,5 @@
+describe('add player to BYB', () => {
+    it('adds player and statistic', () => {
+        cy.visit('https://widgets-bm.dev.digitalsportstech.com/betbuilder?sb=test&demo=test&user=deividas1')
+    })
+})
